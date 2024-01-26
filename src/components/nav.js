64f7481bc955406dcc19c64c3f72d857d97dc8ca
jbin <>
2024-01-26 22:28:41 +0900

    no message

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
-// import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 
 import "./nav.scss"
 
 const Nav = ({ location, title, children }) => {
 
-  const [colorMode, setColorMode] = useState(localStorage.getItem('color-theme'));  
+  const [colorMode, setColorMode] = useState(localStorage.getItem('color-theme'))
+  const [isOpenMenu, setIsOpenMenue] = useState(true)
+  const [isTransition, setIsTransition] = useState(false)
   
-  const handleClick = () => {
+  
+  const handleColor = () => {
     if (colorMode === 'dark') {      
       setColorMode('light')
       localStorage.setItem('color-theme', 'light')
@@ -18,18 +22,79 @@ const Nav = ({ location, title, children }) => {
       document.documentElement.setAttribute('color-theme', 'dark')
     }
     
-  };    
+  }
+
+  const handleMenu = () => {
+    setIsTransition(true)
+    setIsOpenMenue(!isOpenMenu)    
+  }
 
   return (
     <nav className="nav-wrapper">
       <div className="nav-header">
-        <button onClick={handleClick}>
-        {colorMode}
-        </button>
+        <div className="header-left">      
+
+            {colorMode === "dark" ? (
+              <button onClick={handleMenu}>
+                <StaticImage className="menu-icon" src="../images/icons/menu_light_icon.svg" alt="icon"/>
+              </button>      
+            ) : (
+              <button onClick={handleMenu}>
+                <StaticImage className="menu-icon" src="../images/icons/menu_dark_icon.svg" alt="icon"/>
+              </button>     
+            )}    
+
+     
+        </div>
+        <div className='header-center'>
+          {/* <div className="title-text">Jbin</div> */}
+        </div>
+        <div className='header-right'>
+
+        </div>
+
+     
       </div>
-      <div className="sidebar-wrapper">
-        <div className="side-1">b</div>
-        <div className="side-2">c</div>
+      <div className={`sidebar-wrapper ${isOpenMenu && isTransition ? 'open' : ''}${!isOpenMenu  ? 'close' : ''}`}>
+        <div className="side-1">
+          <ul>
+          <li>
+              <Link to="/resume" target="_blank" rel="noopener noreferrer">
+                <StaticImage className="profile-img" src="../images/resume.png" alt="Profile picture"/>
+              </Link>
+            </li>            
+            <li>
+              <Link to="https://glowing-sawine-7a9024.netlify.app/" target="_blank" rel="noopener noreferrer">
+                <StaticImage className="profile-img" src="../images/jbin_interactive_profile.png" alt="Profile picture"/>
+              </Link>
+            </li>
+            <li>
+              <Link to="https://github.com/jbin7" target="_blank" rel="noopener noreferrer">
+                <StaticImage className="profile-img" src="../images/github.png" alt="Profile picture"/>
+              </Link>
+            </li>            
+          </ul>
+        </div>
+        <div className="side-2">
+          <div className='footer'>
+
+            {colorMode === "dark" ? (
+              <button className="color-mode-btn" onClick={handleColor}>
+                <StaticImage className="color-mode-icon" src="../images/icons/light_mode_icon.svg" alt="icon"/>
+                <span>라이트테마</span>
+              </button>
+               
+            ) : (
+              <button className="color-mode-btn" onClick={handleColor}>
+                <StaticImage className="color-mode-icon" src="../images/icons/dark_mode_icon.svg" alt="icon"/>
+                <span>다크테마</span>
+              </button>
+            )}            
+
+             
+          </div>
+                  
+        </div>
       </div>
     </nav>
   )
