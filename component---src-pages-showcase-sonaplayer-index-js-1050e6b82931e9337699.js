c6ec6e75ee5d77cd3e496e41ef21ce1de9714494
jbin <>
2024-02-20 02:27:16 +0900

    Updates

---
+++ component---src-pages-showcase-sonaplayer-index-js-1050e6b82931e9337699.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_blog=self.webpackChunkgatsby_starter_blog||[]).push([[730],{5190:function(e,t,a){a.r(t),a.d(t,{Head:function(){return s}});var l=a(7294),n=a(4e3),r=a(9357),c=a(3192);t.default=e=>{let{data:t,location:a}=e;return(0,l.useEffect)((()=>{}),[]),l.createElement(n.Z,{location:a},l.createElement(c.Z,null))};const s=()=>l.createElement(r.Z,{title:"DJ SONA player"})}}]);
+//# sourceMappingURL=component---src-pages-showcase-sonaplayer-index-js-1050e6b82931e9337699.js.map
