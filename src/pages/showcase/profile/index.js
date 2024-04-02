39690f7a0d4300dfba43d0b139ac2ff60c28356d
jbin <>
2024-04-03 06:49:46 +0900

    no message

--- src/pages/showcase/profile/index.js
+++ src/pages/showcase/profile/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Layout from "../../../components/layout"
 import Seo from "../../../components/seo"
@@ -8,14 +8,32 @@ import "./index.scss"
 
 const Index = ({ data, location }) => {
   
+  const [isReady, setIsReady] = useState(false);
 
-  useEffect(() => {    
-    
-  }, [])   
+  useEffect(() => {
+    window.addEventListener('message', e => {
+      if(e.data.isReady === true) {
+        setIsReady(true)
+      }
+    });
+    return () => {
+
+    };
+  }, []);  
   
   return (
     <Layout location={location}>
-      <div className='interactive-profile-container'>        
+      <div className='interactive-profile-container'>      
+
+        {isReady ? (
+          <div></div>          
+        ) : (
+          <div className='loading-text'>
+          loading..
+          </div>          
+        )}
+
+
         <iframe title="interactive-profile" src="https://glowing-sawine-7a9024.netlify.app/" width="100%"height="100%"></iframe>
       </div>        
     </Layout>
